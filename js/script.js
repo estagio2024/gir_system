f801be13e2bf967a4ebbf30512fc40467f12bfb0
willybrandt-costa <161944331+willybrandt-costa@users.noreply.github.com>
2024-10-11 20:13:29 -0300

    Bloquear plágio

--- js/script.js
+++ js/script.js
@@ -68,3 +68,40 @@ document.addEventListener('DOMContentLoaded', function() {
     // Verifica novamente quando a janela é redimensionada
     window.addEventListener('resize', checkWindowSize);
 });
+
+if (document.addEventListener) {
+    document.addEventListener("contextmenu", function(e) {
+        e.preventDefault();
+        return false;
+    });
+} else { //Versões antigas do IE
+    document.attachEvent("oncontextmenu", function(e) {
+        e = e || window.event;
+        e.returnValue = false;
+        return false;
+    });
+}
+if (document.addEventListener) {
+    document.addEventListener("keydown", bloquearSource);
+} else { //Versões antigas do IE
+    document.attachEvent("onkeydown", bloquearSource);
+}
+
+function bloquearSource(e) {
+    e = e || window.event;
+
+    var code = e.which || e.keyCode;
+
+    if (
+        e.ctrlKey &&
+        (code == 83 || code == 85) //83 = S, 85 = U
+    ) {
+        if (e.preventDefault) {
+            e.preventDefault();
+        } else {
+            e.returnValue = false;
+        }
+
+        return false;
+    }
+}
